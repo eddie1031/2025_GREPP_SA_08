7894a727b382f4221d81004bca205d9b54a87128
eddie1031 <archaco123@gmail.com>
2025-10-28 16:51:56 +0900

    kafka config

--- account-service/src/main/java/io/eddie/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/io/eddie/accountservice/AccountServiceApplication.java
@@ -3,6 +3,9 @@ package io.eddie.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import org.apache.kafka.common.serialization.
+
+
 @SpringBootApplication
 public class AccountServiceApplication {
 
